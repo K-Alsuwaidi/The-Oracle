86ead06636e4f085363158ac5777fc48e448502f
Khalifa Alsuwaidi <76769230+Khalifa-Alsuwaidi@users.noreply.github.com>
2021-03-07 10:20:59 +0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,8 +1,4 @@
-import os
-
 import discord
-
-from dotenv import load_dotenv
 
 from discord.ext import commands
 
@@ -11,8 +7,6 @@ import wikipedia
 import nest_asyncio
 nest_asyncio.apply()
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix = '!!')
 
