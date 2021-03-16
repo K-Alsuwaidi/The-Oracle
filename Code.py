32f9eb15646df0c0c92eee894629c9308d408383
Khalifa Alsuwaidi <76769230+Khalifa-Alsuwaidi@users.noreply.github.com>
2021-03-16 19:37:55 +0400

    Update Code.py
    updated the code, and changed a couple of features

--- Code.py
+++ Code.py
@@ -1,142 +1,172 @@
-
 import discord
 
-from discord.ext import commands
+from discord.ext.commands import Bot
 
 import wikipedia
+import wikipediaapi
+#import modules
+WikiClass = wikipediaapi.Wikipedia(language='en', extract_format=wikipediaapi.ExtractFormat.WIKI) #initiates teh wikipedia class
 
-bot = commands.Bot(command_prefix = '!!')
+Bot = Bot(command_prefix = '~') #sets the prefix
 
-@bot.event
+
+@Bot.event  #on ready function
 async def on_ready():
-    game = discord.Game("Browing Wikipedia, Prefix: !!")
-    await bot.change_presence(status=discord.Status.online, activity=game)
-    print("Online")
+    game = discord.Game("Browing Wikipedia, Prefix: ~")
+    await Bot.change_presence(status=discord.Status.online, activity=game) #sets the status
+    print("Online") #shows that the bot is online
 
-
-@bot.event
-async def on_member_join (member):
-
-        await member.create_dm()
-        await member.dm_channel.send (f'Hi {member.name}, welcome to my Discord server!')
-
-
-
-@bot.event
+@Bot.event #message function
 async def on_message(message):
     Stringo = message.content
-    try:
-        if Stringo[0:2] == '!!':
-            if Stringo == "!!syntax" or Stringo == "!!help": #!!help command or !!syntax, shows the commands
-                help_msg = "Implemented commands: !!search, !!page, !!random, !!image, !!categories, !!link, !!reference, !!section, !!suggest, !!content, !!summary"
-                await message.channel.send(help_msg)
-            elif message.content.startswith("!!search"): #searches about a specific topic
-                search = Stringo[8:]
-                result = wikipedia.search(search, 20)
-                await message.channel.send(result)
+    if Stringo[0:1] == '~':
+        if message.content.startswith("~help"):
+            await message.channel.send("Implement commands: ~help, ~summary, ~search, ~reference, ~random, ~image, ~categories, ~link, ~test, ~page, ~content, ~section, ~sum, and ~url")
 
-            elif message.content.startswith("!!page"): #searches for a specific page
-                search = Stringo[6:]
-                try:
-                    result = wikipedia.page(search, preload=True)
-                    await message.channel.send(result)
-                except wikipedia.exceptions.PageError:
-                    await message.channel.send("Page not found")
+        elif message.content.startswith("~summary"):
+            search = Stringo[9:] #gets the search
+            page = WikiClass.page(search) #makes the page
+            result = page.summary[:500] #gets the summary len 200 chars
+            ListSplited = result.split(".") #spltis the summary to get sentences
+            x = 0
+            StringResult = ""
+            for i in ListSplited: #for loop to add 5 sentences to the result
+                StringResult += i
+                if x == 5:
+                    break
+                x+= 1
+            await message.channel.send(StringResult) #sends result
 
-            elif message.content.startswith("!!random"): #returns a random page
-                tempo = wikipedia.random(pages = 1)
-                result = wikipedia.summary(tempo, 4)
-                await message.channel.send(result)
+        elif message.content.startswith("~search"):
+                search = Stringo[8:] #gets the search
+                result = wikipedia.search(search, 20) #searches for the topics that are connected with the search
+                await message.channel.send(result) #sends result
 
-            elif message.content.startswith("!!image"): #returns an image about the topic
-                search = Stringo[7:]
+
+        elif message.content.startswith("~reference"):
+            search = Stringo[11:]
+            try:
                 WikipediaClass = wikipedia.WikipediaPage(search)
-                result = WikipediaClass.images
-                await message.channel.send(result)
-
-            elif message.content.startswith("!!categories"): #returns the categories about the topic
-                search = Stringo[11:]
-                try:
-                    WikipediaClass = wikipedia.WikipediaPage(search)
-
-                    result = WikipediaClass.categories
-                except:
-                    await message.channel.send('Not found')
-                else:
-                    await message.channel.send(result)
-
-            elif message.content.startswith("!!link"): #returns the link about a topic
-                search = Stringo[6:]
-                WikipediaClass = wikipedia.WikipediaPage(search, preload=True)
-                tempo = WikipediaClass.links
-                result = ""
-                for i in tempo:
-                    if len(result) <= 1960:
-                        result += i
-                        result += ", "
-                    else:
-                        break
-
-
-                await message.channel.send(result[:1999])
-
-            elif message.content.startswith("!!reference"): #return the refrences about a topic
-                search = Stringo[10:]
-                try:
-                    WikipediaClass = wikipedia.WikipediaPage(search)
-                    result = WikipediaClass.references
-                except:
-                    await message.channel.send("Not found")
-                else:
-                    await message.channel.send(result)
-
-            elif message.content.startswith("!!section"): #return the sections about a topic
-                search = Stringo[9: ]
-                WikipediaClass = wikipedia.WikipediaPage(search)
-                result = WikipediaClass.sections
-                await message.channel.send(result)
-
-            elif message.content.startswith("!!suggest"): #return the suggested topic
-                search = Stringo[9:]
-                result = wikipedia.suggest(search)
-                try:
-                    await message.channel.send(result)
-                except:
-                    await message.channel.send("Not valid")
-            elif message.content.startswith("!!content"): #returns what is included in that page, regarding the topic
-                search = Stringo[9:]
-                try:
-                    WikipediaClass = wikipedia.WikipediaPage(search)
-                    result = WikipediaClass.content
-
-                    await message.channel.send(result[0:1999])
-                except:
-                    await message.channel.send("Not valid")
-
-            elif message.content.startswith("!!summary"): #returns a summary, if num provided, it will be established with the user, else, num = 4, returns a summary about the topic.
-                 try:
-                      num = int(Stringo[-1])
-                 except ValueError:
-                     num = 4
-                 finally:
-                      search = Stringo[9:]
-                      result = wikipedia.summary(search, num)
-                      await message.channel.send(result)
-            elif message.content.startswith("!!sum"):
-                 try:
-                      num = int(Stringo[-1])
-                 except ValueError:
-                     num = 4
-                 finally:
-                      search = Stringo[5:]
-                      result = wikipedia.summary(search, num)
-                      await message.channel.send(result[0:1999])
-
+                result = WikipediaClass.references
+            except:
+                await message.channel.send("Not found")
             else:
-                await message.channel.send("Not valid")
-    except:
-        await message.channel.send("An unknown error occured, please report the it to @Uy Scuti#3533")
+                SendResult = result[0:5]
+                await message.channel.send(tuple(SendResult))
 
 
 
-bot.run(TOKEN)
+        elif message.content.startswith("~random"):
+            search = wikipedia.random(pages = 1) #searches for a random page
+            page = WikiClass.page(search) #makes the page
+            result = page.summary[:1000] #gets the summary len 200 chars
+            ListSplited = result.split(".") #spltis the summary to get sentences
+            x = 0
+            StringResult = ""
+            for i in ListSplited: #for loop to add 5 sentences to the result
+                StringResult += i
+                if x == 5:
+                    break
+                x+= 1
+            await message.channel.send(StringResult) #sends result
+
+
+        elif message.content.startswith("~image"):
+            search = Stringo[7:] #search
+            WikipediaClass = wikipedia.WikipediaPage(search) #makes the wikipedia class
+            result = WikipediaClass.images #gets the images
+            for i in result[:len(result)//2]: #loops over the list
+                await message.channel.send(i) #sends the images
+
+
+        elif message.content.startswith("~categories"):
+            search = Stringo[12:] #search
+            try: #if an error arises or the page doesnt exist
+                WikipediaClass = wikipedia.WikipediaPage(search) # makes the wikipedia class
+
+                result = WikipediaClass.categories #gets the categories
+            except: #if error arise
+                await message.channel.send('Page not found') #sends that page not found
+            else:
+                await message.channel.send(result) #send the result
+
+
+        elif message.content.startswith("~link"):
+            search = Stringo[6:]
+            try: #try if an error arises
+                WikipediaClass = wikipedia.page(search) #makes the class
+                result = WikipediaClass.url #gets the url
+                await message.channel.send(result) #sends the result
+            except Exception as e:
+                print(e)
+                await message.channel.send("Page not found") #if error pops up, transfers to this
+
+
+        elif message.content.startswith("~test"):
+            search = Stringo[6:]   #search
+            page = WikiClass.page(search) #makes the class
+            if page.exists(): #checks
+                await message.channel.send("Page exists") #if true print it exists
+            else: #else
+                await message.channel.send("Page doesnt exist") #print it does not exist
+
+
+        elif message.content.startswith("~page"):
+            search = Stringo[6:] #search
+            page = WikiClass.page(search) #class page
+            result = page.text #gets result
+            if len(result) <=2000: #if its acceptable with discord
+                await message.channel.send(result) #sends it if true
+            else: #else
+                for i in range(len(result)//2000): #for loop, divides the result by 2000 to get an integer, a value, if 10000 = 5 and so on
+                    await message.channel.send(result[2000*i:2000*(i+1)]) #for loop and sends the text 2000 per time,
+
+        elif message.content.startswith("~content"):
+            search = Stringo[9:] #search
+            page = WikiClass.page(search) #class page
+            result = page.text #gets result
+            if len(result) <=2000: #if its acceptable with discord
+                await message.channel.send(result) #sends it if true
+            else: #else
+                for i in range(len(result)//2000): #for loop, divides the result by 2000 to get an integer, a value, if 10000 = 5 and so on
+                    await message.channel.send(result[2000*i:2000*(i+1)]) #for loop and sends the text 2000 per time,
+
+
+        elif message.content.startswith("~section"):
+            search = Stringo[9:] #search
+            try: #if an error arises or the page doesnt exist
+                WikipediaClass = wikipedia.WikipediaPage(search) # makes the wikipedia class
+
+                result = WikipediaClass.categories #gets the categories
+            except: #if error arise
+                await message.channel.send('Page not found') #sends that page not found
+            else:
+                await message.channel.send(result) #send the result
+
+
+        elif message.content.startswith("~sum"):
+            search = Stringo[5:] #gets the search
+            page = WikiClass.page(search) #makes the page class
+            result = page.summary[:500] #gets the summary len 200 chars
+            ListSplited = result.split(".") #spltis the summary to get sentences
+            x = 0
+            StringResult = ""
+            for i in ListSplited: #for loop to add 5 sentences to the result
+                StringResult += i
+                if x == 5:
+                    break
+                x += 1
+            await message.channel.send(StringResult) #sends result
+
+
+        elif message.content.startswith("~url"):
+            search = Stringo[5:] #search
+            page = WikiClass.page(search) #makes the page class
+            result = page.fullurl #gets full url
+            await message.channel.send(result) #sends result
+        else:
+            await message.channel.send("Unknown refrence, please write ~help for more options, if you have a suggestion dm @Uy Scuti#3533 in discord") #if command doesnt match any of the above
+
+
+
+Bot.run(TOKEN)
